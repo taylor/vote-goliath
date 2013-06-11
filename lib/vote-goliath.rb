4572acbc7834d7968698b8f2ff4c2eb2129ee857
Wavell Watson <wavell.watson@gmail.com>
2013-06-11 14:04:24 -0500

    made test use server-rb

--- lib/vote-goliath.rb
+++ lib/vote-goliath.rb
@@ -2,51 +2,13 @@
 
 require_relative '../config/environment.rb'
 require 'rubyvote'
-
-module Vote 
-  class API < Grape::API
-    version 'v1', :using => :path
-    format :json
-
-    resource 'plural' do
-      # TEST: curl -D - 'http://localhost:9000/v1/plural'
-      get '/' do
-            [ "The plurality voting system is a single-winner voting system often used to elect executive officers or to elect members of a legislative assembly which is based on single-member constituencies. -- http://en.wikipedia.org/wiki/Instant_Runoff_Voting",
-              "Try curl -D - 'http://localhost:9000/v1/plural/houndstooth,houndstooth,pacha,vuka,vuka,vuka"]
-      end
-
-      # TEST: curl -D - 'http://localhost:9000/v1/plural/houndstooth,houndstooth,pacha,vuka,vuka,vuka'
-      get '/:votes' do
-        votes=params['votes'].split(',')
-        tally = PluralityVote.new(votes)
-        tally.result.winners[0]
-      end
-    end
-  end
-end
-
-
-class APIServer < Goliath::API
-  def response(env)
-    puts env 
-    Vote::API.call(env);
-  end
-end
+require_relative '../server.rb'
 
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'goliath/test_helper' 
 require 'test/unit'
-
-#class PluralVote < Goliath::API
-#  use Goliath::Rack::Params
-#
-#  def response(env)
-#		tally = PluralityVote.new(env.params['votes']) 
-#    [200, {}, tally.result.winners[0]]
-#  end
-#end
 
 class APIServerTest < Test::Unit::TestCase
   include Goliath::TestHelper
